2749d75410c1cf0607c71033aad78eb7bb13ee24
CamilleZoulikian <32639675+CamilleZoulikian@users.noreply.github.com>
2017-11-23 12:03:53 +0100

    adherent dto

---
+++ modeles/adherent.php
@@ -0,0 +1,14 @@
+<?php
+class adherent{
+	private $mail;
+	private $mdp;
+	private $nom;
+	private $prenom;
+
+
+	private $lesMatchs = array();
+
+	public function __construct($mail, $mdp, $nom,$numAdreR, $prenom ){
+
+
+}
