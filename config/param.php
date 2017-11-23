e5444d3d172b4df23a700fe050930dc34371597b
BlackEnergie <the.perrin@outlook.fr>
2017-11-23 11:28:34 +0100

    fichiers de base

---
+++ config/param.php
@@ -0,0 +1,7 @@
+<?php
+class Param {
+	public static $user = 'perrint';
+  	public static $pass = 'perrint';
+	public static $dsn = 'mysql:host=127.0.0.1:1111;dbname=perrint_biorelais;charset=utf8';
+}
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'auto/autoload.php';
+session_start()?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>BioRelais</title>
+        <link rel="icon" href="">
+		<style type="text/css">
+			@import url(styles/bioRelais.css);
+		</style>
+	</head>
+	<body>
+		<?php/*
+			include_once 'controleurs/controleurPrincipal.php';
+		*/?>
+	</body>
+</html>
